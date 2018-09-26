a55eb598400814f98f8c284e156579dc0a863a0d
vitaly <vitaly@3dprinteros.com>
2018-09-27 00:04:00 +0300

    add copy license file to package

--- packaging/osx.cmake
+++ packaging/osx.cmake
@@ -5,6 +5,13 @@ configure_file(${CMAKE_CURRENT_LIST_DIR}/Info.plist.in Info.plist @ONLY)
 
 add_custom_command(
     TARGET packaging POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/packaging/cura_license ${CMAKE_BINARY_DIR}/build/KODAK 3D Slicer.app/Contents/MacOS/
+    COMMAND ${CMAKE_COMMAND} -E rename ${CMAKE_BINARY_DIR}/build/KODAK 3D Slicer.app/Contents/MacOS/cura_license ${CMAKE_BINARY_DIR}/package/LICENSE.txt
+    COMMENT "copying cura_license as LICENSE.txt into package/"
+)
+
+add_custom_command(
+    TARGET packaging POST_BUILD
     COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/build
     COMMAND ${PYTHON_EXECUTABLE} setup.py bdist_mac
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
